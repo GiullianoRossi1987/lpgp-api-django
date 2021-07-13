35dc6875d1a1c58e021db98812ca1080240b2a7f
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2021-07-13 10:53:49 -0300

    Users operations done

--- pages/users/views.py
+++ pages/users/views.py
@@ -6,14 +6,58 @@ from lpgp.entities.Users import User, UsersTable
 from lpgp.connection.Configurations import Configurations
 from django.views.decorators.csrf import csrf_exempt
 from util.default import val_client
+from base64 import b64encode
 
-@csrf_exempt
 def index(request):
     """
 
     """
-    if not val_client(request.POST["client-key"]):
-        raise Exception("Invalid client")
-
-
     return render(request, "forbidden_err.html")
+
+@csrf_exempt
+def login(request):
+    """
+
+    """
+    try:
+        if not val_client(request.POST["client-key"]):
+            raise Exception("Invalid Client")
+        usr_t = UsersTable(Configurations("config.json"))
+        paswd_r = request.POST["password"] if request.POST["encoded"] == "t" else b64encode(request.POST["password"].encode())
+        if usr_t.auth_login(request.POST["name"], paswd_r):
+            dct = {
+                "status": 0,
+                "response": "OK, valid user"
+            }
+        else:
+            dct = {
+                "status": 2,
+                "response": "Ok, invalid user"
+            }
+    except Exception as e:
+        dct = {
+            "status": 1,
+            "response": str(e)
+        }
+    return JsonResponse(dct)
+
+@csrf_exempt
+def get(request):
+    """
+
+    """
+    try:
+        if not val_client(request.POST["client-key"]):
+            raise Exception("Invalid Client")
+        usr_t = UsersTable(Configurations("config.json"))
+        usr_r = User({k: v for k, v in request.POST.items()})
+        dct = {
+            "status": 0,
+            "response": tuple(x.__dict__() for x in usr_t.qr_user(usr_r))
+        }
+    except Exception as e:
+        dct = {
+            "status": 1,
+            "response": str(e)
+        }
+    return JsonResponse(dct)
